055b5043af6ea72ccbbb4ed8d2cfbafa409a8945
Mohammad Akib <mhdakib7769@gmail.com>
2026-09-18 22:19:57 +0600

    Add CORS origin for Netlify app

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ app.include_router(admin.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                  "https://libagent.netlify.app"
+                  ],
+
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
